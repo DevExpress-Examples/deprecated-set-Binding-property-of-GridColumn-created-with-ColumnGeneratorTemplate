da60cf8c6709e41bdb5c6cbbedb41fb57125baf2
Dmitry Romenskiy (DevExpress) <romenskiy.dmitry@corp.devexpress.com>
2018-09-03 16:55:08 +0300

    add example content

---
+++ CS/ColumnBindingExample/ColumnBindingHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace ColumnBindingExample {
+    public class ColumnBindingHelper {
+        public static readonly DependencyProperty BindingPathProperty = DependencyProperty.RegisterAttached("BindingPath", typeof(string), typeof(ColumnBindingHelper), new PropertyMetadata(null, OnBindingPathChanged));
+
+        private static void OnBindingPathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            var column = d as DevExpress.Xpf.Grid.GridColumn;
+            if (column == null || e.NewValue == null)
+                return;
+            column.Binding = new Binding(e.NewValue.ToString());
+        }
+
+        public static string GetBindingPath(DependencyObject obj) {
+            return (string)obj.GetValue(BindingPathProperty);
+        }
+
+        public static void SetBindingPath(DependencyObject obj, string value) {
+            obj.SetValue(BindingPathProperty, value);
+        }
+    }
+}
---
+++ CS/ColumnBindingExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ColumnBindingExample.ViewModels {
+    [POCOViewModel]
+    public class MainViewModel {
+        protected MainViewModel() {
+            Data = new ObservableCollection<Dictionary<string, object>>();
+            for (int i = 0; i < 25; i++)
+                Data.Add(new Dictionary<string, object>() { { "ID", i }, { "Name", "Name" + i }, { "Date", DateTime.Now.Date }, { "Check", false } });
+            Columns = new ObservableCollection<ColumnViewModel>();
+            Columns.Add(new ColumnViewModel() { Header = "ID", FieldName = "[ID]" });
+            Columns.Add(new ColumnViewModel() { Header = "Name", FieldName = "[Name]" });
+            Columns.Add(new ColumnViewModel() { Header = "Date", FieldName = "[Date]" });
+            Columns.Add(new ColumnViewModel() { Header = "Check", FieldName = "[Check]" });
+        }
+
+        public virtual ObservableCollection<ColumnViewModel> Columns { get; set; }
+        public virtual ObservableCollection<Dictionary<string, object>> Data { get; set; }
+    }
+
+    public class ColumnViewModel {
+        public string FieldName { get; set; }
+        public string Header { get; set; }
+    }
+}
